f874495fd023084ebdc1211263d0727e0c3ab698
Wes <wesleyayer92@gmail.com>
2020-01-30 16:17:51 -0500

    adds gangsterize functionality

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Gangsterize from './Gangsterize';
+
+function App() {
+  return (
+    <div>
+      <Gangsterize name="Wes" />
+      <Gangsterize name="Cam" />
+      <Gangsterize name="Alex" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Gangsterize.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Gangsterize({name}) {
+    return (
+        <p>{name} becomes: lil {name}!</p>
+    )
+}
+
+export default Gangsterize;
